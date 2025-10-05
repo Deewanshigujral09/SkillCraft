0dde24b58ad4870cf9750422542e6a3a53dab58f
Deewanshi Gujral <gujraldeewanshi09@gmail.com>
2025-10-05 12:52:00 +0530

    Your update message

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SctWD3 from "./Components/Quiz/sct_wd_3.jsx";
+
+const App = () => {
+  return (
+    <>
+      <SctWD3 />
+    </>
+  );
+};
+
+export default App;
